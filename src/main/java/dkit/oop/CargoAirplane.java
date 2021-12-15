1dec18ac484cfb3482816d149e6749264aa7b7a1
TheBogman02 <92158821+TheBogman02@users.noreply.github.com>
2021-12-15 11:19:11 +0000

    Question 1

--- src/main/java/dkit/oop/CargoAirplane.java
+++ src/main/java/dkit/oop/CargoAirplane.java
@@ -1,16 +1,32 @@
 package dkit.oop;
 
-public class CargoAirplane {
+import java.util.ArrayList;
+
+public class CargoAirplane extends Airplane{
 
     // fields (ref. Q1)
-
-
+private final int MAX_LOAD_KILOS;
+private int currentLoad;
+ArrayList<String> passengerList;
     // constructor
+
+    public CargoAirplane(String type, int maxLoad) {
+        super(type);
+        this.MAX_LOAD_KILOS = maxLoad;
+        this.currentLoad = 0;
+    }
 
 
     // toString()
 
 
-
+    @Override
+    public String toString() {
+        return "CargoAirplane{" +
+                "MAX_LOAD_KILOS=" + MAX_LOAD_KILOS +
+                ", currentLoad=" + currentLoad +
+                ", passengerList=" + passengerList +
+                '}';
+    }
 } // END of CargoAirplane class.
 
